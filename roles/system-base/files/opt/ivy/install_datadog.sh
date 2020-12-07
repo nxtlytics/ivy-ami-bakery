e3049b608ef9b1a3a5bfd34be1426f190e42f2a5
Ricardo Rosales <missingcharacter@gmail.com>
2020-12-07 10:16:55 -0600

    Install New Relic Infra and datadog agents

--- roles/system-base/files/opt/ivy/install_datadog.sh
+++ roles/system-base/files/opt/ivy/install_datadog.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+if [ -z "${DD_API_KEY+x}" ]; then
+  DD_API_KEY='ivy-use-set-datadog-key'
+  echo "DD_API_KEY was not set so I set it to ${DD_API_KEY}"
+else
+  echo "DD_API_KEY is ${DD_API_KEY}"
+fi
+
 if [[ -d /etc/datadog-agent ]]; then
     echo "Datadog agent installed already."
     exit
 fi
 
-DD_AGENT_MAJOR_VERSION=7 DD_INSTALL_ONLY=true bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
+DD_AGENT_MAJOR_VERSION=7 DD_INSTALL_ONLY=true \
+    bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
 sed -i 's/# bind_host:.*/bind_host: 0\.0\.0\.0/' /etc/datadog-agent/datadog.yaml
 
 cat <<EOF > /etc/datadog-agent/conf.d/network.d/conf.yaml.default
