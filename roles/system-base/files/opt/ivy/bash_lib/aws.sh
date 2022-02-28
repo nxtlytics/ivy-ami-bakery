62a7914130010a0dad3b7513866214f40190a597
Ricardo Rosales <728243+missingcharacter@users.noreply.github.com>
2022-02-28 00:04:14 -0600

    Adding pre-commit hooks

--- roles/system-base/files/opt/ivy/bash_lib/aws.sh
+++ roles/system-base/files/opt/ivy/bash_lib/aws.sh
@@ -90,9 +90,9 @@ function get_eni_id() {
   local TAG
   TAG=$(get_ivy_tag)
   aws ec2 describe-network-interfaces --region "$(get_region)" \
-                                      --filters Name=tag:"${TAG}:sysenv",Values="$(get_sysenv)" \
-                                                Name=tag:"${TAG}:role",Values="${ENI_ROLE}" \
-                                                Name=tag:"${TAG}:service",Values="${SERVICE}" \
+                                      --filters "Name=tag:${TAG}:sysenv,Values=$(get_sysenv)" \
+                                                "Name=tag:${TAG}:role,Values=${ENI_ROLE}" \
+                                                "Name=tag:${TAG}:service,Values=${SERVICE}" \
                                       --query 'NetworkInterfaces[0].NetworkInterfaceId' \
                                       --output text
 }
@@ -140,142 +140,149 @@ function get_eni_interface() {
 }
 
 function attach_eni() {
-    local INSTANCE_ID=$1
-    local ENI_ID=$2
+  local INSTANCE_ID="${1}"
+  local ENI_ID="${2}"
+  local OLD_INTERFACE REGION status attachment_id NEW_INTERFACE
 
-    local OLD_INTERFACE=$(get_default_interface)
-    local REGION=$(get_region)
-    local ENI_IP=$(get_eni_ip ${ENI_ID})
-    local status=$(aws ec2 describe-network-interfaces --region ${REGION} \
-                   --network-interface-ids ${ENI_ID} \
-                   --query 'NetworkInterfaces[0].Status' --output text)
+  OLD_INTERFACE=$(get_default_interface)
+  REGION=$(get_region)
+  status=$(aws ec2 describe-network-interfaces --region "${REGION}" \
+                 --network-interface-ids "${ENI_ID}" \
+                 --query 'NetworkInterfaces[0].Status' --output text)
 
-    if [ "${status}" == "in-use" ]; then
-      local attachment_id=$(aws ec2 describe-network-interfaces --region ${REGION} \
-                           --network-interface-ids ${ENI_ID} \
-                           --query 'NetworkInterfaces[0].Attachment.AttachmentId' --output text)
-      aws ec2 detach-network-interface --region ${REGION} \
-                                       --attachment-id ${attachment_id} --force
-    fi
+  if [ "${status}" == "in-use" ]; then
+    attachment_id=$(aws ec2 describe-network-interfaces --region "${REGION}" \
+                         --network-interface-ids "${ENI_ID}" \
+                         --query 'NetworkInterfaces[0].Attachment.AttachmentId' \
+                         --output text)
+    aws ec2 detach-network-interface --region "${REGION}" \
+                                     --attachment-id "${attachment_id}" \
+                                     --force
+  fi
 
-    until [ "${status}" == "available" ]; do
-        status=$(aws ec2 describe-network-interfaces --region ${REGION} \
-                 --network-interface-ids ${ENI_ID} \
-                 --query 'NetworkInterfaces[0].Status' --output text) || sleep 1
-        sleep 1
-    done
+  until [ "${status}" == "available" ]; do
+      status=$(aws ec2 describe-network-interfaces --region "${REGION}" \
+               --network-interface-ids "${ENI_ID}" \
+               --query 'NetworkInterfaces[0].Status' \
+               --output text) || sleep 1
+      sleep 1
+  done
 
-    aws ec2 attach-network-interface --region ${REGION} \
-                                     --instance-id ${INSTANCE_ID} \
-                                     --network-interface-id ${ENI_ID} \
-                                     --device-index 1
+  aws ec2 attach-network-interface --region "${REGION}" \
+                                   --instance-id "${INSTANCE_ID}" \
+                                   --network-interface-id "${ENI_ID}" \
+                                   --device-index 1
 
-    until [ "${status}" == "in-use" ]; do
-        status=$(aws ec2 describe-network-interfaces --region ${REGION} \
-                 --network-interface-ids ${ENI_ID} \
-                 --query 'NetworkInterfaces[0].Status' --output text) || sleep 1
-        sleep 1
-    done
+  until [ "${status}" == "in-use" ]; do
+      status=$(aws ec2 describe-network-interfaces --region "${REGION}" \
+               --network-interface-ids "${ENI_ID}" \
+               --query 'NetworkInterfaces[0].Status' \
+               --output text) || sleep 1
+      sleep 1
+  done
 
-    local NEW_INTERFACE=$(get_eni_interface ${ENI_ID})
+  NEW_INTERFACE=$(get_eni_interface "${ENI_ID}")
 
-    until /sbin/ip link show dev ${NEW_INTERFACE} &>/dev/null; do
-        sleep 1
-    done
+  until /sbin/ip link show dev "${NEW_INTERFACE}" &>/dev/null; do
+      sleep 1
+  done
 
-    echo "Attachment: region=${REGION}, instance-id=${INSTANCE_ID}, eni=${ENI_ID}"
+  echo "Attachment: region=${REGION}, instance-id=${INSTANCE_ID}, eni=${ENI_ID}"
 
-    until [ "$(</sys/class/net/${NEW_INTERFACE}/operstate)" == "up" ]; do
-        sleep 1
-    done
+  until [ "$(</sys/class/net/"${NEW_INTERFACE}"/operstate)" == "up" ]; do
+      sleep 1
+  done
 
-    sed -i -e 's/ONBOOT=yes/ONBOOT=no/' "/etc/sysconfig/network-scripts/ifcfg-${OLD_INTERFACE}"
-    sed -i -e 's/BOOTPROTO=dhcp/BOOTPROTO=none/' "/etc/sysconfig/network-scripts/ifcfg-${OLD_INTERFACE}"
-    ifdown "${OLD_INTERFACE}"
-    sed -e "s/${OLD_INTERFACE}/${NEW_INTERFACE}/g" "/etc/sysconfig/network-scripts/route-${OLD_INTERFACE}" >> "/etc/sysconfig/network-scripts/route-${NEW_INTERFACE}"
-    echo > "/etc/sysconfig/network-scripts/route-${OLD_INTERFACE}"
-    service network restart
-
+  sed -i -e 's/ONBOOT=yes/ONBOOT=no/' "/etc/sysconfig/network-scripts/ifcfg-${OLD_INTERFACE}"
+  sed -i -e 's/BOOTPROTO=dhcp/BOOTPROTO=none/' "/etc/sysconfig/network-scripts/ifcfg-${OLD_INTERFACE}"
+  ifdown "${OLD_INTERFACE}"
+  sed -e "s/${OLD_INTERFACE}/${NEW_INTERFACE}/g" "/etc/sysconfig/network-scripts/route-${OLD_INTERFACE}" >> "/etc/sysconfig/network-scripts/route-${NEW_INTERFACE}"
+  echo > "/etc/sysconfig/network-scripts/route-${OLD_INTERFACE}"
+  service network restart
 }
 
 function attach_ebs() {
-    # Forcibly attaches a volume to an instance
-    local INSTANCE_ID=$1
-    local VOLUME_ID=$2
-    local DEVICE=$3
+  # Forcibly attaches a volume to an instance
+  local INSTANCE_ID="${1}"
+  local VOLUME_ID="${2}"
+  local DEVICE="${3}"
+  local REGION vol_state attached_instance_id status
 
-    local REGION=$(get_region)
-    local vol_state=$(aws ec2 describe-volumes --region ${REGION} \
-                        --volume-ids ${VOLUME_ID} \
+  REGION=$(get_region)
+  attached_instance_id=""
+  if ! vol_state=$(aws ec2 describe-volumes --region "${REGION}" \
+                --volume-ids "${VOLUME_ID}" \
+                --query 'Volumes[0].Attachments' \
+                --output text)
+  then
+    echo "ERROR: Volume ${VOLUME_ID} not found"
+    return 1
+  fi
+
+  if [ -n "${vol_state}" ]; then
+    # Volume is currently attached. Detach if necessary
+    attached_instance_id="$(awk '{print $4}' <<<"${vol_state}")"
+    if [ "${attached_instance_id}" == "${INSTANCE_ID}" ]; then
+      echo "Volume ${VOLUME_ID} is already attached to ${INSTANCE_ID}"
+      return 0
+    fi
+    # forcibly detach
+    echo "Detaching ${VOLUME_ID}..."
+    if ! aws ec2 detach-volume --region "${REGION}" \
+        --force \
+        --volume-id "${VOLUME_ID}"
+    then
+      echo "ERROR: Detaching volume ${VOLUME_ID}."
+      return 1
+    fi
+    until [ -z "${vol_state}" ]; do
+      vol_state=$(aws ec2 describe-volumes --region "${REGION}" \
+                          --volume-ids "${VOLUME_ID}" \
+                          --query 'Volumes[0].Attachments' \
+                          --output text)
+      echo "Waiting on detaching ${VOLUME_ID}..."
+      sleep 1
+    done
+  fi
+
+  # Attach
+  echo "Attaching ${VOLUME_ID}..."
+  if ! aws ec2 attach-volume --region "${REGION}" \
+      --instance-id "${INSTANCE_ID}" \
+      --volume-id "${VOLUME_ID}" \
+      --device "${DEVICE}"
+  then
+    echo "ERROR: Attaching volume ${VOLUME_ID}."
+    return 1
+  fi
+  until [ "${attached_instance_id}" == "${INSTANCE_ID}" ]; do
+    vol_state=$(aws ec2 describe-volumes --region "${REGION}" \
+                        --volume-ids "${VOLUME_ID}" \
                         --query 'Volumes[0].Attachments' --output text)
-    local attached_instance_id=""
-
-    if [ $? -ne 0 ]; then
-        echo "ERROR: Volume ${VOLUME_ID} not found"
-        return 1
-    fi
-
-    if [ -n "${vol_state}" ]; then
-        # Volume is currently attached. Detach if necessary
-        attached_instance_id=$(echo ${vol_state} | awk '{print $4}')
-        local status=$(echo ${vol_state} | awk '{print $5}')
-        if [ "${attached_instance_id}" == "${INSTANCE_ID}" ]; then
-            echo "Volume ${VOLUME_ID} is already attached to ${INSTANCE_ID}"
-            return 0
-        fi
-        # forcibly detach
-        echo "Detaching ${VOLUME_ID}..."
-        aws ec2 detach-volume --region ${REGION} --force --volume-id ${VOLUME_ID}
-        if [ $? -ne 0 ]; then
-            echo "ERROR: Detaching volume ${VOLUME_ID}."
-            return 1
-        fi
-        until [ -z "${vol_state}" ]; do
-            local vol_state=$(aws ec2 describe-volumes --region ${REGION} \
-                                --volume-ids ${VOLUME_ID} \
-                                --query 'Volumes[0].Attachments' --output text)
-            echo "Waiting on detaching ${VOLUME_ID}..."
-            sleep 1
-        done
-    fi
-
-    # Attach
-    echo "Attaching ${VOLUME_ID}..."
-    aws ec2 attach-volume --region ${REGION} \
-        --instance-id ${INSTANCE_ID} \
-        --volume-id ${VOLUME_ID} \
-        --device ${DEVICE}
-    if [ $? -ne 0 ]; then
-        echo "ERROR: Attaching volume ${VOLUME_ID}."
-        return 1
-    fi
-    until [ "${attached_instance_id}" == "${INSTANCE_ID}" ]; do
-        local vol_state=$(aws ec2 describe-volumes --region ${REGION} \
-                            --volume-ids ${VOLUME_ID} \
-                            --query 'Volumes[0].Attachments' --output text)
-        local attached_instance_id=$(echo ${vol_state} | awk '{print $4}')
-        echo "Waiting on attaching ${VOLUME_ID}..."
-        sleep 1
-    done
-    until [ -b "${DEVICE}" ]; do
-        echo "Waiting for Linux to recognize ${DEVICE}..."
-        sleep 1
-    done
-    echo "Volume ${VOLUME_ID} attached to ${INSTANCE_ID} at ${DEVICE}."
-    return 0
+    attached_instance_id=$(awk '{print $4}' <<<"${vol_state}")
+    echo "Waiting on attaching ${VOLUME_ID}..."
+    sleep 1
+  done
+  until [ -b "${DEVICE}" ]; do
+    echo "Waiting for Linux to recognize ${DEVICE}..."
+    sleep 1
+  done
+  echo "Volume ${VOLUME_ID} attached to ${INSTANCE_ID} at ${DEVICE}."
+  return 0
 }
 
 function update_route53() {
-    local DEFAULT_INTERFACE=$(get_default_interface)
-    local INTERNAL_IP=$(get_ip_from_interface ${DEFAULT_INTERFACE})
+  local DEFAULT_INTERFACE INTERNAL_IP TXN_FILE
+  DEFAULT_INTERFACE=$(get_default_interface)
+  INTERNAL_IP=$(get_ip_from_interface "${DEFAULT_INTERFACE}")
 
-    local HOSTED_ZONE_ID=$1
-    local RRSET_NAME=$2
-    local IP=${3-$INTERNAL_IP} # Override IP address if specified
+  local HOSTED_ZONE_ID="${1}"
+  local RRSET_NAME="${2}"
+  local IP="${3-$INTERNAL_IP}" # Override IP address if specified
 
-    local TXN_FILE=$(mktemp -t -u "r53-dns-transaction.XXXX.json")
+  TXN_FILE=$(mktemp -t -u "r53-dns-transaction.XXXX.json")
 
-    cat <<EOF > ${TXN_FILE}
+  cat <<EOF > "${TXN_FILE}"
 {
   "Comment": "Update ${RRSET_NAME}",
   "Changes": [
@@ -296,8 +303,8 @@ function update_route53() {
 }
 EOF
 
-    aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch file://${TXN_FILE}
-    rm -f ${TXN_FILE}
+  aws route53 change-resource-record-sets --hosted-zone-id "${HOSTED_ZONE_ID}" --change-batch file://"${TXN_FILE}"
+  rm -f "${TXN_FILE}"
 }
 
 function get_ssm_param() {
@@ -313,6 +320,7 @@ function get_ssm_param() {
 function get_secret() {
   local SECRET_ID="${1}"
   local REGION="${2:-$(get_region)}"
-  local VALUE=$(aws secretsmanager --region "${REGION}" get-secret-value --secret-id "${SECRET_ID}" | jq --raw-output .SecretString)
-  echo ${VALUE}
+  local VALUE
+  VALUE=$(aws secretsmanager --region "${REGION}" get-secret-value --secret-id "${SECRET_ID}" | jq --raw-output .SecretString)
+  echo "${VALUE}"
 }
