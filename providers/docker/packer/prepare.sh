e8f4e3f268ae565f54529ac0afd6a9aed7dee9d1
Ricardo Rosales <728243+missingcharacter@users.noreply.github.com>
2022-03-22 15:15:46 -0500

    ivy-base on fedora:35 works!

--- providers/docker/packer/prepare.sh
+++ providers/docker/packer/prepare.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
-set -x
+set -xeuo pipefail
+IFS=$'\n\t'
 
 # Upgrade the base image fully
 # TODO: discuss potentially disabling this after building base to prevent blindly sliding package versions from build to build
 yum -y update
 
 # Install dev tools and python
-yum install -y wget python2-devel python3-devel libffi libffi-devel openssl-devel python3-pip sudo
+yum install -y wget python-rpm-macros python3-rpm python3-devel libffi libffi-devel openssl-devel python3-pip sudo
 yum groupinstall -y 'Development Tools'
+
+# ansible expects python at /usr/bin/python NOT at /bin/python
+ln -s "$(command -v python3)" /usr/bin/python
 
 # Allow notty sudo
 sed -n -e '/Defaults.*requiretty/s/^/#/p' /etc/sudoers
 
 # Upgrade pip
-if [[ ! -f get-pip.py ]]; then
-    wget https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py
-fi
+pip3 install --upgrade pip
 
 # Install ansible
-pip3 install --upgrade --trusted-host pypi.python.org ansible==2.10.0
+pip3 install --upgrade --trusted-host pypi.python.org ansible==5.4.0
