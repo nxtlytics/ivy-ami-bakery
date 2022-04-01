2ba23828f63c8d7c326d54b75c1d47abc9b0290a
Ricardo Rosales <728243+missingcharacter@users.noreply.github.com>
2022-03-31 20:00:31 -0500

    [qemu] shellcheck updates

--- test.sh
+++ test.sh
@@ -2,7 +2,9 @@
 set -x
 set -e
 
+# shellcheck disable=SC1091
 source ./providers/universal/images/default/packer.env
+# shellcheck disable=SC1091
 source ./providers/universal/images/ivy-kubernetes-1.21/packer.env
 
 ./bin/packer build ./providers/universal/packer/universal-qemu.pkr.hcl
