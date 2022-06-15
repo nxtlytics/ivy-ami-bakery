c244b7ade3576f9ccdab1e4b4a7e7566af30509b
Tommy Walsh <thomas.walsh@over-haul.com>
2022-06-15 10:38:54 -0500

    update to python3.8 so that ansible can run

--- providers/amazon/packer/prepare.sh
+++ providers/amazon/packer/prepare.sh
@@ -4,6 +4,10 @@ set -x
 # Allow notty sudo
 sed -n -e '/Defaults.*requiretty/s/^/#/p' /etc/sudoers
 
+# install python 3.8
+yum install -y amazon-linux-extras
+amazon-linux-extras enable python3.8
+
 # Upgrade the base image fully
 # TODO: discuss potentially disabling this after building base to prevent blindly sliding package versions from build to build
 yum -y update
@@ -14,7 +18,7 @@ yum groupinstall -y 'Development Tools'
 
 # Upgrade pip
 if [[ ! -f get-pip.py ]]; then
-    wget https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py
+    wget https://bootstrap.pypa.io/get-pip.py && python3.8 get-pip.py
 fi
 
 # Install ansible
