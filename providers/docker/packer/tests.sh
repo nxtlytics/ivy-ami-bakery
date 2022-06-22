8e411aec288123823ce76de797ca6e21b8d1ac46
Ricardo Rosales <ricardo.rosales@over-haul.com>
2022-06-21 20:39:24 -0500

    [P20-1666] [docker/packer] tests.sh now exit 1 if datadog-version is not locked or installed datadog version is not the correct version

--- providers/docker/packer/tests.sh
+++ providers/docker/packer/tests.sh
@@ -6,8 +6,10 @@ DATADOG_VERSION='7.33.1'
 
 if ! grep -q  "${DATADOG_VERSION}" <(yum versionlock list); then
   echo "Datadog is NOT locked to version ${DATADOG_VERSION}"
+  exit 1
 fi
 
 if ! grep -q "${DATADOG_VERSION}" <(datadog-agent version); then
   echo "Installed datadog is NOT version ${DATADOG_VERSION}"
+  exit 1
 fi
